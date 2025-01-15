591aca54dd6f8f9df1e361756cbc33f0aba5cea3
tonjej <tonje.jorgensen93@gmail.com>
2025-01-15 13:53:15 +0100

    quotes funker, må legge inn sitater

--- script.mjs
+++ script.mjs
@@ -4,6 +4,15 @@ import HTTP_CODES from './utils/httpCodes.mjs';
 const server = express();
 const port = (process.env.PORT || 8000);
 
+const quotes = [
+    "quote1", 
+    "quote2", 
+    "quote3",
+    "quote4",
+    "quote5",
+    "quote6"
+    ];
+
 server.set('port', port);
 server.use(express.static('public'));
 
@@ -11,11 +20,18 @@ function getRoot(req, res, next) {
     res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
 }
 
+function getQuote(){
+    return quotes[Math.floor(Math.random() * quotes.length)];
+}
 
 server.get("/", getRoot);
 
 server.get("/tmp/poem", (req, res) => {
-    res.send("I eat my peas with honey. I've done it all my life. It makes the peas taste funny. But it keeps them on the knife!");
+    res.send('I eat my peas with honey <br> I\'ve done it all my life <br> It makes the peas taste funny <br> But it keeps them on the knife!');
+})
+
+server.get("/tmp/quote", (req, res) => {
+    res.send(getQuote());
 })
 
 server.listen(server.get('port'), function () {
