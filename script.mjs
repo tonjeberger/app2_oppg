5e6941cce88466b69bddc1345162f5c97ea3cfe5
tonjej <tonje.jorgensen93@gmail.com>
2025-01-15 13:11:01 +0100

    server aumatisk oppdatering virker med nodemon

--- script.mjs
+++ script.mjs
@@ -2,7 +2,7 @@ import express from 'express'
 import HTTP_CODES from './utils/httpCodes.mjs';
 
 const server = express();
-const port = (process.env.PORT || 8080);
+const port = (process.env.PORT || 8000);
 
 server.set('port', port);
 server.use(express.static('public'));
@@ -11,8 +11,21 @@ function getRoot(req, res, next) {
     res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
 }
 
+
 server.get("/", getRoot);
+
+server.get("/tmp/poem", (req, res) => {
+    res.send("Dikt her");
+})
 
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
 });
+
+// server.get("/tmp/poem", function (){
+//     console.log("heihei", server.get('port'));
+// })
+
+// server.get("/tmp/poem", getPoem);
+
+// server.listen("/tmp/poem", getPoem);
