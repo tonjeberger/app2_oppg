90b5be8a8ad758f232a44f3c83dae51616280566
tonjej <tonje.jorgensen93@gmail.com>
2025-01-17 11:04:13 +0100

    post virker, kjører det i postman

--- script.mjs
+++ script.mjs
@@ -41,6 +41,24 @@ server.get("/tmp/quote", (req, res) => {
 
 
 
+
+server.post('/tmp/sum/:a/:b', (req, res) => {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+    const sum = a + b;
+    res.send(sum.toString());
+});
+
+
+server.listen(server.get('port'), function () {
+    console.log('server running', server.get('port'));
+});
+
+
+
+
+
+
 // //post requests
 // server.get("/tmp/sum/:a/:b", (req, res) => {
     //     res.send(req.params.a + req.params.b);
@@ -51,23 +69,14 @@ server.get("/tmp/quote", (req, res) => {
         // // })
 
         
-server.get('/tmp/sum/:a/:b', (req, res) => {
-    //res.send("add two numbers separeted by a '/' in the url to add them together");
-    const a = parseInt(req.params.a);
-    const b = parseInt(req.params.b);
-    const sum = a + b;
-    res.send(sum.toString());
-})
+// server.get('/tmp/sum/:a/:b', (req, res) => {
+//     //res.send("add two numbers separeted by a '/' in the url to add them together");
+//     const a = parseInt(req.params.a);
+//     const b = parseInt(req.params.b);
+//     const sum = a + b;
+//     res.send(sum.toString());
+// })
 
-server.post('/tmp/sum/:a/:b', (req, res) => {
-    const a = parseInt(req.params.a);
-    const b = parseInt(req.params.b);
-    const sum = a + b;
-    res.send(sum.toString());
-    console.log("parameters added")
-});
-
-
-server.listen(server.get('port'), function () {
-    console.log('server running', server.get('port'));
-});
+// let getPost = fetch("http://localhost:8000/tmp/sum")
+//     .then(response => response.text())
+//     .then(data => console.log(data));
