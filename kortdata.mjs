39cb166d2ac12a541e72e2bfc71da9860cafa037
tonjej <tonje.jorgensen93@gmail.com>
2025-01-27 12:20:04 +0100

    changed names to match png-files to make it easier to show the correct card

--- kortdata.mjs
+++ kortdata.mjs
@@ -1,4 +1,4 @@
 
-export const suits = ["Hjerter", "Ruter", "Spar", "Kløver"];
+export const suits = ["hearts", "diamonds", "spades", "clubs"];
 
-export const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+export const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"];
