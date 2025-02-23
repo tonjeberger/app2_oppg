6fbf89e97be22eaf0e870ef1723205cbe8678b85
tonjej <tonje.jorgensen93@gmail.com>
2025-02-23 12:48:17 +0100

    importing api-route to init

--- init/init.mjs
+++ init/init.mjs
@@ -1,7 +1,7 @@
 import {Tree, Node, saveTree} from "../data/tree.mjs"
 //import {console} from "inspector"; // copilot sier at vi ikke trenger denne importen
 import fs from "fs/promises";
-
+import treeRouter from '../routes/treeAPI.mjs';
 
 let dummyTreeData = await fs.readFile("./init/dummy/tree1.json");
 dummyTreeData = JSON.parse(dummyTreeData);
@@ -10,10 +10,10 @@ console.log(dummyTreeData)
 // har laget tree.json, må kanskje hente ut den dataen
 // slik som over, så kan jeg fjerne den fra tree.mjs?
 // må finne ut hvordan jeg skal koble det til serveren fra denne filen
-
+server.use("/tree", treeRouter);
 server.get("/", (req, res, next) => {
     res.json(dummyTreeData);
-    console.log("tree");
+    console.log("treeeeeee");
 });
 
 //___________start server_________
