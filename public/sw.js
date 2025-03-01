025f3ccb2bafd4d0bf17d0ade9fc6944dca4813f
tonjej <tonje.jorgensen93@gmail.com>
2025-03-01 15:36:09 +0100

    added missing code from lecture to files, and started adding a form for a note-taking app

--- public/sw.js
+++ public/sw.js
@@ -14,8 +14,25 @@ self.addEventListener("install", (evt) => {
         const cache = await caches.open(cacheID);
         console.log("[ServiceWorker] Caching all: app shell and content");
         await cache.addAll(contentToCache);
-    }))
+    })());
 })
 
 self.addEventListener("fetch", (evt) => {// alle nettverkskall går gjennom fetch
+  if(!(
+    evt.request.url.startsWith("http:") ||
+    evt.request.url.startsWith("https:")
+  ))
+  {return;
+
+  };
+  evt.respondWith((async () => {
+    const r = await caches.match(evt.request);
+    console.log(`[Service Worker] Fetching resource: ${evt.request.url}`);
+    if (r) {return r};
+    const response = await fetch(evt.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${evt.request.url}`);
+    cache.put(evt.request, response.clone());
+    return response;
+  })());
 });
