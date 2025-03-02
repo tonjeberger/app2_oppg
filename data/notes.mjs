9c63ca1a78e3a327b92880a09d37bf843b65e0a2
tonjej <tonje.jorgensen93@gmail.com>
2025-03-02 11:28:45 +0100

    added date to notes-objects contents/details

--- data/notes.mjs
+++ data/notes.mjs
@@ -2,6 +2,7 @@ export const addNote = (title, content) => {
     notes.push({
         id: notes.length + 1,
         title,
+        date: new Date(),
         content
     })
 }
@@ -13,9 +14,13 @@ const notes =[
     {
         id: 1,
         title: 'Note 1',
-        content: 'Content of note 1'},
+        date: "02-03-2025",
+        content: 'Content of note 1'
+    },
     {
         id: 2,
         title: 'Note 2',
-        content: 'Content of note 2'}
-]
+        date: "02-03-2025",
+        content: 'Content of note 2'
+    }
+];
