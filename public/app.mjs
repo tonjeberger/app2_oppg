c309660ab4ce9c7d22b6e698c6de1afa69bc499a
tonjej <tonje.jorgensen93@gmail.com>
2025-03-05 12:32:09 +0100

    pasted most of the code into external files, need to get a clearer view of what each file should contain

--- public/app.mjs
+++ public/app.mjs
@@ -1,18 +1,53 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/sw.js"); // Register the service worker, js-filen. dette er ikke en modul, så den har vi bare kalr .js
 } // kan sees på som ett event som lytter etter en serviceWorker, og når den finner en så registrerer den den
+// import sessionFileStore from "session-file-store";
 //navigator -> browser
 //serviceWorker -> en unik bckground worker som kan kjøre i bakgrunnen, kan bare kjøre en SW
 
 import * as api from "../routes/noteAPI.mjs";
+// import {notes} from "../data/notes.mjs";
+
+const notes =[
+    {
+        id: 1,
+        title: 'Note 1',
+        date: "02-03-2025",
+        content: 'Content of note 1'
+    },
+    {
+        id: 2,
+        title: 'Note 2',
+        date: "02-03-2025",
+        content: 'Content of note 2'
+    }
+];
 
 const noteContainer = document.getElementById("note-container");
 const newNoteBtn = document.getElementById("new-note-btn");
+const noteForm = document.createElement("form");
 
+const form = `
+    <form id="note-form" display="block">
+        <input type="text" name="title" placeholder="Title"><br>
+        <textarea name="content" rows="15" cols="70" placeholder="Your note"></textarea><br>
+        <input id="add-note" type="submit" value="Add note"></input>
+    </form>`;
+
+    
 newNoteBtn.addEventListener("click", async () => {
-    // this button should open a form
     console.log("New note button clicked");
-    let noteDiv = document.createElement("div");
-    noteDiv.innerHTML += `<h3>New notes here when api is ready</h3>`;
-    noteContainer.appendChild(noteDiv);
-    });
+    noteContainer.innerHTML = form;
+    noteContainer.appendChild(noteForm);
+    let note = await api.createNote(formData);
+    noteContainer.appendChild(note);
+
+});
+
+noteForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    console.log(notes);
+    console.log("Note created" + note);
+
+});
+
