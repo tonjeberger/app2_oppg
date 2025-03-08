c04dc5b4f1fa3354c1a973ad8a698a765b696a37
tonjej <tonje.jorgensen93@gmail.com>
2025-03-08 18:02:16 +0100

    api for client, fetching from/ sending to the server

--- utils/fetchAPI.mjs
+++ utils/fetchAPI.mjs
@@ -3,3 +3,20 @@
 const url = "http://localhost:8000/"
 
 
+async function getNote(id){
+    const response = await fetch(url + "notes/" + id, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    const note = await response.json();
+    return note;
+}
+
+async function newNote(noteForm){};
+
+async function editNote(id){}
+
+async function deleteNote(id){};
