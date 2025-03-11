588d708b438925860e9aebfcb72f9d24220e1954
tonjej <tonje.jorgensen93@gmail.com>
2025-03-11 21:07:52 +0100

    turned function into class to extend properly

--- data/recordStoreAbstractInterface.mjs
+++ data/recordStoreAbstractInterface.mjs
@@ -1,16 +1,31 @@
-
-function RecordStoreAbstractInterface(){
-    return {
-        create,
-        read,
-        update,
-        purge
+class RecordStoreAbstractInterface {
+    create(note) {
+        throw new Error("'create' is not implemented");
+    }
+    update(note) {
+        throw new Error("'update' is not implemented");
+    }
+    read(note) {
+        throw new Error("'read' is not implemented");
+    }
+    purge(note) {
+        throw new Error("'purge' is not implemented");
     }
 }
 
-function create(note){throw new Error(" 'create' is not implemented")};
-function update(note){throw new Error("'update' is not implemented")};
-function read(note){throw new Error("'read' is not implemented")};
-function purge(note){throw new Error("'delete' is not implemented")};
+
+// function RecordStoreAbstractInterface(){
+//     return {
+//         create,
+//         read,
+//         update,
+//         purge
+//     }
+// }
+
+// function create(note){throw new Error(" 'create' is not implemented")};
+// function update(note){throw new Error("'update' is not implemented")};
+// function read(note){throw new Error("'read' is not implemented")};
+// function purge(note){throw new Error("'delete' is not implemented")};
 
 export default RecordStoreAbstractInterface;
