1b1f38118e0712e9f2e043f4837398340ed5386d
tonjej <tonje.jorgensen93@gmail.com>
2025-03-11 16:43:57 +0100

    made NoteStore methods async with return await

--- data/noteStore.mjs
+++ data/noteStore.mjs
@@ -5,20 +5,20 @@ import dbManager from "./db.mjs";
 class NoteStore extends RecordStoreAbstractInterface {
 
     // her skal sql-spørringen inn
-    create(note){
-        dbManager.create(`INSERT INTO public."Note_table"(title, note) VALUES ($2, $3) RETURNING id, title, note;`, [note.title, note.noteData]);
+    async create(note){
+        return await dbManager.create(`INSERT INTO public."Note_table"(title, note) VALUES ($2, $3) RETURNING id, title, note;`, [note.title, note.noteData]);
     }
 
-    read(note){
-        dbManager.read(`SELECT id, title, note FROM public."Note_table" WHERE id=$1; `, [note.id]);
+    async read(note){
+        return await dbManager.read(`SELECT id, title, note FROM public."Note_table" WHERE id=$1; `, [note.id]);
     }
 
-    update(note){
-        dbManager.update(`UPDATE public."Note_table" SET title=$1, note=$2 WHERE id=$3 RETURNING id, title, note;`, [note.title, note.noteData, note.id]); 
+    async update(note){
+        return await dbManager.update(`UPDATE public."Note_table" SET title=$1, note=$2 WHERE id=$3 RETURNING id, title, note;`, [note.title, note.noteData, note.id]); 
     }
 
-    purge(note){
-        dbManager.purge(`DELETE FROM public."Note_table" WHERE id=$1;`, [note.id]);
+    async purge(note){
+        return await dbManager.purge(`DELETE FROM public."Note_table" WHERE id=$1;`, [note.id]);
     }
 
 }
