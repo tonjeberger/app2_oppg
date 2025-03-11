4e04e8ba8efb1221efbcd414d7f3884842740824
tonjej <tonje.jorgensen93@gmail.com>
2025-03-11 21:07:12 +0100

    fixed placeholders in sql

--- data/noteStore.mjs
+++ data/noteStore.mjs
@@ -6,7 +6,7 @@ class NoteStore extends RecordStoreAbstractInterface {
 
     // her skal sql-spørringen inn
     async create(note){
-        return await dbManager.create(`INSERT INTO public."Note_table"(title, note) VALUES ($2, $3) RETURNING id, title, note;`, [note.title, note.noteData]);
+        return await dbManager.create(`INSERT INTO public."Note_table"(title, note) VALUES ($1, $2) RETURNING id, title, note;`, [note.title, note.content]);
     }
 
     async read(note){
@@ -14,7 +14,7 @@ class NoteStore extends RecordStoreAbstractInterface {
     }
 
     async update(note){
-        return await dbManager.update(`UPDATE public."Note_table" SET title=$1, note=$2 WHERE id=$3 RETURNING id, title, note;`, [note.title, note.noteData, note.id]); 
+        return await dbManager.update(`UPDATE public."Note_table" SET title=$1, note=$2 WHERE id=$3 RETURNING id, title, note;`, [note.title, note.content, note.id]); 
     }
 
     async purge(note){
