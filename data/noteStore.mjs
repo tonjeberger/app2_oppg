0e90ee8bae0434530729ce097fab5638d4c8e7a3
tonjej <tonje.jorgensen93@gmail.com>
2025-03-12 09:56:39 +0100

    removed [ ] from the arguments in dbManager methods

--- data/noteStore.mjs
+++ data/noteStore.mjs
@@ -7,19 +7,19 @@ class NoteStore extends RecordStoreAbstractInterface {
     // her skal sql-spørringen inn
     async create(note){
         console.log("NoteStore får inn: " + note.title, note.content);
-        return await dbManager.create(`INSERT INTO public."Note_table"(title, note) VALUES ($1, $2) RETURNING id, title, note;`, [note.title, note.content]);
+        return await dbManager.create(`INSERT INTO public."Note_table"(title, content) VALUES ($1, $2) RETURNING id, title, content;`, note.title, note.content);
     }
 
     async read(note){
-        return await dbManager.read(`SELECT id, title, note FROM public."Note_table" WHERE id=$1; `, [note.id]);
+        return await dbManager.read(`SELECT id, title, content FROM public."Note_table" WHERE id=$1; `, note.id);
     }
 
     async update(note){
-        return await dbManager.update(`UPDATE public."Note_table" SET title=$1, note=$2 WHERE id=$3 RETURNING id, title, note;`, [note.title, note.content, note.id]); 
+        return await dbManager.update(`UPDATE public."Note_table" SET title=$1, content=$2 WHERE id=$3 RETURNING id, title, content;`, note.title, note.content, note.id); 
     }
 
     async purge(note){
-        return await dbManager.purge(`DELETE FROM public."Note_table" WHERE id=$1;`, [note.id]);
+        return await dbManager.purge(`DELETE FROM public."Note_table" WHERE id=$1;`, note.id);
     }
 
 }
