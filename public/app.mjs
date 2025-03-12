b2ffd8f09e7d8d44b93af69a767b8843962b3402
tonjej <tonje.jorgensen93@gmail.com>
2025-03-12 11:25:54 +0100

    adding div with note-title to test

--- public/app.mjs
+++ public/app.mjs
@@ -5,6 +5,7 @@ if ("serviceWorker" in navigator) {
 import { newNote, getNote, updateNote, deleteNote } from "./apiHandler.mjs";
 
 const noteForm = document.getElementById("note-form")
+const noteContainer = document.getElementById("note-container");
 
 
 // sende inn skjema for ny note
@@ -12,6 +13,9 @@ noteForm.addEventListener("submit", async (evt) => {
     evt.preventDefault();
     const formData = new FormData(noteForm);
     const note = await newNote(formData);
+    let date = new Date();
+    date = date.toISOString();
+    noteContainer.innerHTML += `<div><h2>Title: ${note.title}</h2> <button id="open-note">open note</button></div>`; // denne skal flyttes til et view
     console.log(note);
     console.log("Note created");
 });
