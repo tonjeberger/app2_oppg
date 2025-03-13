a03ab1ccc8ec58dd2a1f49ae65467b1fef5d694f
tonjej <tonje.jorgensen93@gmail.com>
2025-03-13 23:25:06 +0100

    added try/catch to fetchTemplate, small change in what is returned. cloneTemplate: removed console logs/some comments, added a div to put template clone in, changed return to the template div instead of clone

--- public/modules/templateManager.mjs
+++ public/modules/templateManager.mjs
@@ -1,58 +1,50 @@
 const TemplateManager = {};
 
 TemplateManager.fetchTemplate = async (path) => { // funksjon som hører til TemplateManager, path vil være url/filbane
-
-    //laster inn ekstern template
-    let rawTemplate = await (await fetch(path)).text(); // vil dette være html-filen? .text() gjør det om til text og det er dette rawTemplate vil inneholde
-    //et hack for å gjøre det enklere å bruke templates dynamisk
-    let div = document.createElement("div");
-    div.innerHTML = rawTemplate; // her blir teksten fra rawTemplate som ble hentet over lagt inn i html
-    let template = div.firstChild;
-    return template;
+    try {
+        //laster inn ekstern template
+        let response = await fetch(path);
+        let rawTemplate = await response.text(); // vil dette være html-filen? .text() gjør det om til text og det er dette rawTemplate vil inneholde
+        
+        let div = document.createElement("div");
+        div.innerHTML = rawTemplate; // her blir teksten fra rawTemplate som ble hentet over lagt inn i html
+        
+        
+        let template = div.querySelector("template");
+        return template;
+        
+    } catch (error) {
+        console.error("Error fetching template", error);
+    }
 
 }
 
+
+
 TemplateManager.cloneTemplate = (template, target, data = {}) => {
+    console.log("cloneTemplate ", template);
     const clone = template.content.cloneNode(true);
-    let html = clone.innerHTML;
     console.log("cloneTemplate ", clone);
-    
-    // for(let key of Object.keys(data)){
-    //     // html = html.replaceAll(RegExp(`/\{\{${key}\}\}/gm`, data[key]));
-    //     html = html.replace(new RegExp(`\\{\\{${key}\\}\\}`, 'g'), data[key]);
-    // }// må se mer på denne koden når jeg ser forelesningen på nytt
     for (let key of Object.keys(data)) {
-        console.log("key ", key);
-        console.log("data ", data);
-        console.log("data[key] ", data[key]);
+        
         const note = clone.querySelector(`#note-${key}`);
         if(note){
             note.textContent = data[key];
-            html = note.textContent;
+            console.log("set ", key, "to", data[key]);
         }
-        console.log("note ", note);
-        // html = html.replaceAll(RegExp(`/\{\{${key}\}\}/gm`, data[key]));
     }
 
-    clone.innerHTML = html;
-    target.appendChild(clone);
-    console.log("cloneTemplate again ", clone);
-    return clone;
+    const templateContainer = document.createElement("div");
+    templateContainer.className = "template-container";
+
+    while (clone.firstChild) {
+        templateContainer.appendChild(clone.firstChild);
+    }
+    
+    target.innerHTML = "";
+    target.appendChild(templateContainer);
+    console.log("Template container: ", clone);
+    return templateContainer;
 }
 
-// TemplateManager.cloneTemplate = (template, target, data = {}) => {
-//     const clone = template.content.cloneNode(true);
-
-//     // Iterate over the keys in the data object
-//     for (let key of Object.keys(data)) {
-//         // Find the element with the corresponding ID and set its text content
-//         const element = clone.querySelector(`#note-${key}`);
-//         if (element) {
-//             element.textContent = data[key];
-//         }
-//     }
-
-//     target.appendChild(clone);
-//     return clone;
-// }
 export default TemplateManager;
