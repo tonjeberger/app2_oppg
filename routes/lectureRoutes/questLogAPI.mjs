06293e158116b592b546f6247d77f334f46b0a64
tonjej <tonje.jorgensen93@gmail.com>
2025-03-13 23:35:26 +0100

    deleted old files

--- routes/lectureRoutes/questLogAPI.mjs
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-const questLogRouter = express.Router();
-
-questLogRouter.use(express.json());
-
-export default questLogRouter;
--- routes/lectureRoutes/userAPI.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const userRouter = express.Router();
-
-
-
-userRouter.use(express.json());
-
-export default userRouter;
